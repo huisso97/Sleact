5a82c77436ac33673f1d24e2218f3bcbeb47001d
최소희 <chosohi@naver.com>
2022-10-13 08:39:38 +0900

    Feat: login css 적용 및 회원가입 제출 시, 에러 발생

--- alecture/pages/LogIn/test.tsx
+++ alecture/pages/LogIn/test.tsx
@@ -1,55 +1,40 @@
-// import useInput from '@hooks/useInput';
-import { Success, Form, Error, Label, Input, LinkContainer, Button, Header } from '@pages/SignUp/styles';
 import fetcher from '@utils/fetcher';
-import axios from 'axios';
 import React, { useCallback, useState } from 'react';
-import { Link, Redirect } from 'react-router-dom';
 import useSWR from 'swr';
+import { Success, Form, Error, Label, Input, LinkContainer, Button, Header } from '@pages/SignUp/styles';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 const LogIn = () => {
-  const { data, error, mutate } = useSWR('/api/users', fetcher);
-
+  const { data, error } = useSWR('/api/users', fetcher);
   const [logInError, setLogInError] = useState(false);
-  // const [email, onChangeEmail] = useInput('');
-  // const [password, onChangePassword] = useInput('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const onSubmit = useCallback(
-    (e) => {
+    (e: React.FormEvent<HTMLFormElement>) => {
       e.preventDefault();
       setLogInError(false);
       axios
         .post(
           '/api/users/login',
-          { email, password },
+          {
+            email,
+            password,
+          },
           {
             withCredentials: true,
           },
         )
-        .then((response) => {
-          // revalidate();
-        })
-        .catch((error) => {
-          setLogInError(error.response?.data?.statusCode === 401);
-        });
+        .then((res) => {});
     },
     [email, password],
   );
-
   if (data === undefined) {
-    return <div>로딩중...</div>;
+    return <div>로딩중~</div>;
   }
-
-  // if (data) {
-  //   return <Redirect to="/workspace/sleact/channel/일반" />;
-  // }
-
-  // console.log(error, userData);
-  // if (!error && userData) {
-  //   console.log('로그인됨', userData);
-  //   return <Redirect to="/workspace/sleact/channel/일반" />;
-  // }
-
+  if (data) {
+    return;
+  }
   return (
     <div id="container">
       <Header>Sleact</Header>
@@ -57,13 +42,13 @@ const LogIn = () => {
         <Label id="email-label">
           <span>이메일 주소</span>
           <div>
-            <Input type="email" id="email" name="email" />
+            <Input type="email" id="email" name="email" value={email} onChange={() => setEmail} />
           </div>
         </Label>
         <Label id="password-label">
           <span>비밀번호</span>
           <div>
-            <Input type="password" id="password" name="password" />
+            <Input type="password" id="password" name="password" value={password} onChange={() => setPassword} />
           </div>
           {logInError && <Error>이메일과 비밀번호 조합이 일치하지 않습니다.</Error>}
         </Label>
